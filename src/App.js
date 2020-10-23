a885e759958f94562a14632b2d41d3a9842f4fe2
Muhammad magdy <58988660+muhammadmb@users.noreply.github.com>
2020-10-23 18:14:22 +0200

    Add Home Page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import About from './Components/About/About.js';
 import NavBar from './Components/NavBar/NavigationBar.js';
+import Home from './Components/Home/Home.js'
 import BookCard from './Components/BookCard/BookCard.js'
 import {BrowserRouter,Route,Switch} from 'react-router-dom';
-
+import './App.css'
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <NavBar/>
-        <BookCard/>
-        <BookCard/>
-        <BookCard/>
-        <BookCard/>
-        <BookCard/>
-        <BookCard/>
+        <Home/>
+        {/* <BookCard/> */}
       </BrowserRouter>
     </div>
   );
