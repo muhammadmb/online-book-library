838a50d7149a2fe2c05ca8696935a0f0365f27d8
Muhammad magdy <58988660+muhammadmb@users.noreply.github.com>
2020-10-30 22:09:11 +0200

    add footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import '../../Style.css'
+import {Icons, FooterDiv, HR} from './FooterStyle'
 
 const Footer = () => {
   
     return(
-        <div>
-            <text>footer</text>
-        </div>
+        <>
+            <div>
+                <HR/>
+                <FooterDiv  >
+                    <text style = {{fontSize : "20px", color:"white"}}>Follow Us </text>
+                    <Icons target='_blank' href="http://www.facebook.com" title = "Facebook" className="fa fa-facebook"></Icons>
+                    <Icons target='_blank' href="http://www.twitter.com" title = "Twitter" className="fa fa-twitter"></Icons>
+                    <Icons target='_blank' href="http://www.youtube.com" title = "Youtub" className="fa fa-youtube"></Icons>
+                    <Icons target='_blank' href="http://www.linkedin.com" title = "LinkedIn" className="fa fa-linkedin"></Icons>
+                </FooterDiv>
+            </div>
+        </>
     );
 }
 
--- src/Components/Footer/FooterStyle.js
+++ src/Components/Footer/FooterStyle.js
@@ -4,7 +4,19 @@ export const FooterDiv = styled.div `
     text-align: center;
     padding: 10px 0;
 `
-
-export const Footerp = styled.p `
-    color: #888
+export const Icons = styled.a `
+    margin:6px;
+    font-size: 20px;
+    color: #1fa6b9;
+        &:hover{
+            text-decoration:"";
+            color:#0d3647;
+        }
+`
+export const HR = styled.hr
+`
+    color:white;
+    width:90%;
+    background-color:white;
+    height:3px;
 `
