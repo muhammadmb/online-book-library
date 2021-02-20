33b9758835d94ded12b4d8f6c1caea3c24ef9129
muhammadmb <muhammadmagdyje@gmail.com>
2021-02-20 22:38:04 +0200

    refactore NavBar and make it responsive

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import {Icons, FooterDiv, HR} from './FooterStyle'
 
 const Footer = () => {
   
     return(
         <>
-            <div>
-                <HR/>
-                <FooterDiv  >
-                    <text style = {{fontSize : "20px", color:"white"}}>Follow Us </text>
-                    <Icons target='_blank' href="http://www.facebook.com" title = "Facebook" className="fa fa-facebook"></Icons>
-                    <Icons target='_blank' href="http://www.twitter.com" title = "Twitter" className="fa fa-twitter"></Icons>
-                    <Icons target='_blank' href="http://www.youtube.com" title = "Youtub" className="fa fa-youtube"></Icons>
-                    <Icons target='_blank' href="http://www.linkedin.com" title = "LinkedIn" className="fa fa-linkedin"></Icons>
-                </FooterDiv>
+            <div className="container">
+                <hr/>
+                <div>
+                    <span style = {{fontSize : "20px", color:"white"}}>Follow Us </span>
+                    <a target='_blank' href="http://www.facebook.com" title = "Facebook" className="fa fa-facebook"></a>
+                    <a target='_blank' href="http://www.twitter.com" title = "Twitter" className="fa fa-twitter"></a>
+                    <a target='_blank' href="http://www.youtube.com" title = "Youtub" className="fa fa-youtube"></a>
+                    <a target='_blank' href="http://www.linkedin.com" title = "LinkedIn" className="fa fa-linkedin"></a>
+                </div>
             </div>
         </>
     );
--- src/Components/NavBar/NavigationBar.js
+++ src/Components/NavBar/NavigationBar.js
@@ -1,31 +1,51 @@
 import React from 'react';
 import '../../Style.css';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
 import {Link} from 'react-router-dom';
 
 const NavigationBar = () => {
   
     return(
-        <>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">BookReaders</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Link className = "NavLinks" to="/">Home</Link>
-                    <Link className = "NavLinks" to="/authors">Authors</Link>
-                    <Link className = "NavLinks" to="/books">Books</Link>
-                    <Link className = "NavLinks" to="/connect">Connect</Link>
-                    <Link className = "NavLinks" to="/about">About</Link>
-                </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                {/* <Button variant="outline-info">Search</Button> */}
-                </Form>
-            </Navbar>
-        </>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+
+                <button className="navbar-toggler" 
+                    type="button" 
+                    data-toggle="collapse" 
+                    data-target="#navbarTogglerDemo01" 
+                    aria-controls="navbarTogglerDemo01" 
+                    aria-expanded="false" 
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <Link className="navbar-brand" to="/">BookReaders</Link>
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/Authors">Authors</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/Books">Books</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/Connect">Connect</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/About">About</Link>
+                    </li>
+                    </ul>
+                    <form className="form-inline my-2 my-lg-0">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+                    <button className="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+
     );
 }
 
