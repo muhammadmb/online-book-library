2daeec2c3a5fd0c7bc0168b272c709c7f422104e
muhammadmb <muhammadmagdyje@gmail.com>
2021-02-23 00:32:00 +0200

    supporting responsive in about page

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -18,12 +18,12 @@ const About = () => {
                         <img src="https://images.unsplash.com/photo-1580904578549-0d13d5a12653?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=341&q=80"/>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget purus pulvinar, feugiat turpis sed, feugiat ipsum. Pellentesque dictum venenatis nibh in lobortis. Quisque orci metus, porttitor viverra dictum sed, bibendum id ex. Praesent ornare libero nec ligula dignissim sodales. Morbi sodales aliquam purus, ac tincidunt erat interdum in.</p>
                         <h4>Ahmed Tarek</h4>
-                        <span>SEO</span>
+                        <span>CEO</span>
                     </div>
                     <div className="card blogCard">
-                        <h2>We are ready to know about your experiance.</h2>
+                        <h2>Share your experiance with us.</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget purus pulvinar, feugiat turpis sed, feugiat ipsum. Pellentesque dictum venenatis nibh in lobortis. </p>
-                        <Link><button className="blogButton">BR Blog</button></Link>
+                        <Link to="/blog"><button className="blogButton">BR Blog</button></Link>
                     </div>
                     <div className="Clear"></div>
                 </div>
