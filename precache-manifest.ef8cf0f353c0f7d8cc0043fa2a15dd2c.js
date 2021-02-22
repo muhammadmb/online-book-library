d8e03cec796baa285d9a5c568367bd0b3e23a25e
muhammadmb <muhammadmagdyje@gmail.com>
2021-02-22 23:57:24 +0200

    Updates

---
+++ precache-manifest.ef8cf0f353c0f7d8cc0043fa2a15dd2c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07eeedbf7c9e02156611da5ada9db3f2",
+    "url": "/online-book-library/index.html"
+  },
+  {
+    "revision": "fd74a64770beb7e092cb",
+    "url": "/online-book-library/static/css/main.cef27227.chunk.css"
+  },
+  {
+    "revision": "187e862b5ed878d970a8",
+    "url": "/online-book-library/static/js/2.ea1fd4dc.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/online-book-library/static/js/2.ea1fd4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd74a64770beb7e092cb",
+    "url": "/online-book-library/static/js/main.e7c11321.chunk.js"
+  },
+  {
+    "revision": "5a35952177d07a8aef98",
+    "url": "/online-book-library/static/js/runtime-main.c90527d5.js"
+  },
+  {
+    "revision": "3101d6e24482c5e4c3097145577ea3e0",
+    "url": "/online-book-library/static/media/AuthorPage.3101d6e2.jpg"
+  },
+  {
+    "revision": "88b5a25621ce9f821e790b1e50621466",
+    "url": "/online-book-library/static/media/contact.88b5a256.png"
+  },
+  {
+    "revision": "e1dcc292c1976a929c2aff7e0302adfc",
+    "url": "/online-book-library/static/media/libraryBackground.e1dcc292.jpg"
+  }
+]);
