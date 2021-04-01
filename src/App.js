5b6eaf38329a8e2cf588148ead76ec26283c9d98
muhammadmb <muhammadmagdyje@gmail.com>
2021-04-01 22:18:56 +0200

    Revert "Add light mode and change search image"
    This reverts commit 71995e9f65c3e7a3253bc3179f207a23bff11abc.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React from 'react';
+import './App.css'
 import About from './Components/About/About.js';
 import NavBar from './Components/NavBar/NavigationBar.js';
 import Home from './Components/Home/Home.js';
 import Footer from './Components/Footer/Footer.js';
 import BookPage from './Components/BooksPage/BooksPage';
-import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Contact from './Components/Connect/ContactUs.js';
-
-import './App.css'
-import authorPage from './Components/AuthorsPage/authorsPage.js';
-import AuthorInformation from './Components/AuthorInformationPage/AuthorInformation.js';
-
+import AuthorPage from './Components/AuthorsPage/AuthorsPage.js';
+import BookInfo from './Components/BookInfoPage/BookInfo.js';
+import NotFoundPage from './Components/NotFoundPage/NotFoundPage.js';
+import Genres from './Components/GenresPage/Genres';
+import AuthorInfo from './Components/AuthorInfoPage/AuthorInfo';
+import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <NavBar/>
-          <Switch>
-          <Route exact path="/" component = {Home}/>
-            <Route exact path="/online-book-library" component = {Home}/>
-            <Route exact path="/Home" component = {Home}/>
-            <Route exact path="/books" component = {BookPage}/>
-            <Route exact path="/authors" component = {authorPage}/>
-            <Route exact path="/Connect" component = {Contact}/>
-            <Route exact path="/about" component = {About}/>
-            <Route exact path="/AuthorInformation" component = {AuthorInformation}/>
-          </Switch>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/online-book-library" component={Home} />
+          <Route exact path="/Home" component={Home} />
+          <Route exact path="/books" component={BookPage} />
+          <Route exact path="/authors" component={AuthorPage} />
+          <Route exact path="/Connect" component={Contact} />
+          <Route exact path="/about" component={About} />
+          <Route path="/genre/:genreId/books/:id" component={BookInfo} />
+          <Route exact path="/genre/:id" component={Genres} />
+          <Route exact path="/Authors/:Id" component={AuthorInfo} />
+          <Route exact path="/search/:SearchQuery" component={Search} />
+          <Route exact path="/search" component={Search} />
+          <Route path="/*" component={NotFoundPage} />
+        </Switch>
         <Footer />
       </BrowserRouter>
     </div>
--- src/Components/AuthorsPage/authorsPage.js
+++ src/Components/AuthorsPage/authorsPage.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import DataServices from '../API/DataServices/DataServices';
 import AuthorCard from '../AuthorCard/AuthorCard';
+import './authorPageStyle.css';
+import Skeleton from '@material-ui/lab/Skeleton';
 
-const authorPage = () =>{
+const AuthorPage = () => {
+
+    const [isLoad, setIsLoad] = useState(false);
+    const [authorData, setAuthorData] = useState([]);
+
+    useEffect(() => {
+        const GetAuthors = async () => {
+            const result = await DataServices.GetAuthors();
+            setAuthorData(result.data);
+            setIsLoad(true);
+        }
+
+        GetAuthors();
+    }, [])
+
     return (
-        <div style = {{width : 1170, margin : "auto", display : "flex", flexWrap : "wrap"}}>
-            
-            <AuthorCard />
-            <AuthorCard />
-            <AuthorCard />
-            <AuthorCard />
-            <AuthorCard />
-            <AuthorCard />
-            <AuthorCard />
-            <AuthorCard />
-            <AuthorCard />
+        <div className="container">
+
+            {
+                isLoad ?
+                    <div className="AuthorPage">
+                        {
+                            authorData.map((item) => (
+                                <AuthorCard
+                                    key={item.id}
+                                    src={item.pictureUrl}
+                                    name={item.name}
+                                    description={item.bio}
+                                    dateOfBirth={item.dateOfBirth.substring(0, 4)}
+                                    dateOfDeath={item.dateOfDeath != null ? item.dateOfDeath.substring(0, 4) !== "0001" ? item.dateOfDeath.substring(0, 4) : "" : ""}
+                                    page={`/Authors/${item.id}`}
+                                />
+                            ))
+                        }
+                    </div>
+                    :
+                    <div className="AuthorPage">
+                        <Skeleton className="skeleton" variant="rect" width={335} height={540} />
+                        <Skeleton className="skeleton" variant="rect" width={335} height={540} />
+                        <Skeleton className="skeleton" variant="rect" width={335} height={540} />
+                        <Skeleton className="skeleton" variant="rect" width={335} height={540} />
+                    </div>
+            }
+
         </div>
-        
+
     );
 }
 
-export default authorPage;
+export default AuthorPage;
