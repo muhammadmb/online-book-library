acf303d4db1fe1d9016372b625b6cbece7b11a0b
muhammadmb <muhammadmagdyje@gmail.com>
2022-03-11 12:51:50 +0200

    Redesign Search page

--- src/Components/API/DataServices/DataServices.js
+++ src/Components/API/DataServices/DataServices.js
@@ -3,32 +3,35 @@ import { API_URL } from '../APIConstants';
 
 class DataServices {
 
-    GetGenres(PageNumber, PageSize, fields) {
-        return axios.get(`${API_URL}/Genres?fields=${fields}&PageNumber=${PageNumber}&PageSize=${PageSize}`);
+    async GetGenres(PageNumber, PageSize, fields) {
+        return await axios.get(`${API_URL}/Genres?fields=${fields}&PageNumber=${PageNumber}&PageSize=${PageSize}`);
     }
 
-    GetGenre(id, fields) {
-        return axios.get(`${API_URL}/Genres/${id}?fields=${fields}`);
+    async GetGenre(id, fields) {
+        return await axios.get(`${API_URL}/Genres/${id}?fields=${fields}`);
     }
 
-    GetBooksByGenre(parameters) {
+    async GetBooksByGenre(parameters) {
         const { genreId, fields, sortby, yearOfPublish, pageNumber, pageSize, author } = parameters;
-        return axios.get(
+        return await axios.get(
             `${API_URL}/Genres/${genreId ? genreId : '00000000-0000-0000-0000-000000000000'}/Books?${author ? `author=${author}` : ''}${fields ? `&fields=${fields}` : ''}${pageNumber ? `&PageNumber=${pageNumber}` : ''}${pageSize ? `&PageSize=${pageSize}` : ''}${sortby ? `&sortby=${sortby}` : ''}${yearOfPublish ? `&YearOfPublish=${yearOfPublish}` : ''}`
         )
     }
 
-    GetBookSearch(fields, SearchQuery) {
-        return axios.get(`${API_URL}/Genres/00000000-0000-0000-0000-000000000000/Books?fields=${fields}&SearchQuery=${SearchQuery}`);
+    async GetBookSearch(parameters) {
+        const { fields, SearchQuery, pageNumber, pageSize } = parameters;
+        return await axios.get(
+            `${API_URL}/Genres/00000000-0000-0000-0000-000000000000/Books?${fields ? `fields=${fields}` : ''}${SearchQuery ? `&SearchQuery=${SearchQuery}` : ''}${pageNumber ? `&PageNumber=${pageNumber}` : ''}${pageSize ? `&PageSize=${pageSize}` : ''}`
+        );
     }
 
-    GetBookDetails(genreId, bookId) {
-        return axios.get(`${API_URL}/Genres/${genreId}/Books/${bookId}`);
+    async GetBookDetails(genreId, bookId) {
+        return await axios.get(`${API_URL}/Genres/${genreId}/Books/${bookId}`);
     }
 
-    GetReviews(parameters) {
+    async GetReviews(parameters) {
         const { genreId, bookId, fields, pageNumber, pageSize } = parameters;
-        return axios.get(`${API_URL}/Genres/${genreId}/Books/${bookId}/reviews?${fields ? `fields=${fields}` : ''}${pageNumber ? `&PageNumber=${pageNumber}` : ''}${pageSize ? `&PageSize=${pageSize}` : ''}`);
+        return await axios.get(`${API_URL}/Genres/${genreId}/Books/${bookId}/reviews?${fields ? `fields=${fields}` : ''}${pageNumber ? `&PageNumber=${pageNumber}` : ''}${pageSize ? `&PageSize=${pageSize}` : ''}`);
     }
 
     async CreateCommentToPost(genreId, bookId, reviewerName, email, reviewDescription, bookRate) {
@@ -48,16 +51,17 @@ class DataServices {
         });
     }
 
-    GetAuthor(authorId) {
-        return axios.get(`${API_URL}/Authors/${authorId}`);
+    async GetAuthor(authorId) {
+        return await axios.get(`${API_URL}/Authors/${authorId}`);
     }
 
-    GetAuthors() {
-        return axios.get(`${API_URL}/Authors?fields=id,name,bio,pictureUrl,dateOfBirth,dateOfDeath`);
+    async GetAuthors() {
+        return await axios.get(`${API_URL}/Authors?fields=id,name,bio,pictureUrl,dateOfBirth,dateOfDeath`);
     }
 
-    GetAuthorsSearch(SearchQuery) {
-        return axios.get(`${API_URL}/Authors?fields=id,name,bio,pictureUrl,dateOfBirth,dateOfDeath&SearchQuery=${SearchQuery}`);
+    async GetAuthorsSearch(parameters) {
+        const { fields, SearchQuery, pageNumber, pageSize } = parameters;
+        return await axios.get(`${API_URL}/Authors?${fields ? `fields=${fields}` : ''}&SearchQuery=${SearchQuery}${pageNumber ? `&PageNumber=${pageNumber}` : ''}${pageSize ? `&PageSize=${pageSize}` : ''}`);
     }
 
     async UpdateVote(genreId, bookId, reviewId, vote, path) {
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,88 +1,159 @@
 import React, { useEffect, useState } from 'react';
 import DataServices from '../API/DataServices/DataServices';
 import AuthorCard from '../AuthorCard/AuthorCard';
-import BookCard from '../BookCard/BookCard';
-import notFoundImg from '../../Images/search.png';
-import './Search.css'
+import BookCard from '../Cards/BookCard/BookCard';
+import GenreList from '../GenreList/GenreList';
+import './Search.css';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Search = (props) => {
 
     const SearchQuery = props.match.params.SearchQuery;
 
     const [books, setBooks] = useState([]);
+    const [booksHeaders, setBooksHeaders] = useState([]);
     const [authors, setAuthors] = useState([]);
-    const theme = localStorage.getItem("theme");
+    const [authorHeaders, setAuthorHeaders] = useState([]);
+    const bookxPagination = booksHeaders["x-pagination"] !== undefined ? JSON.parse(booksHeaders["x-pagination"]) : {};
+    const authorxPagination = authorHeaders["x-pagination"] !== undefined ? JSON.parse(authorHeaders["x-pagination"]) : {};
+    const { Dark } = useSelector((state) => state.Theme);
+    const [booksPageNumber, setBookPageNumber] = useState(1);
+    const [authorsPageNumber, setAuthorPageNumber] = useState(1);
 
     useEffect(() => {
+
         const GetBooks = async () => {
-            const result = await DataServices.GetBookSearch("id, booktitle, bookCover, genre, author", `${SearchQuery}`);
+            const parameters = {
+                fields: "id,booktitle,bookCover,genre,bookRating,author",
+                SearchQuery: SearchQuery,
+                pageNumber: booksPageNumber,
+                pageSize: 5
+            }
+            const result = await DataServices.GetBookSearch(parameters);
             setBooks(result.data);
+            setBooksHeaders(result.headers);
         }
 
+        if (SearchQuery !== undefined) {
+            GetBooks();
+        }
+    }, [SearchQuery, booksPageNumber]);
+
+    useEffect(() => {
         const GetAuthors = async () => {
-            const result = await DataServices.GetAuthorsSearch(`${SearchQuery}`);
-            setAuthors(result.data);
-        }
 
-        GetBooks();
-        GetAuthors();
-    }, [SearchQuery])
+            const parameters = {
+                fields: "id,name,bio,pictureUrl,dateOfBirth,dateOfDeath",
+                SearchQuery: SearchQuery,
+                pageNumber: authorsPageNumber,
+                pageSize: 4
+            }
+
+            const result = await DataServices.GetAuthorsSearch(parameters);
+            setAuthors(result.data);
+            setAuthorHeaders(result.headers);
+        }
+        if (SearchQuery !== undefined) {
+            GetAuthors();
+        }
+    }, [SearchQuery, authorsPageNumber])
 
     return (
         <div className="container">
+            <div className={Dark ? "search-result" : "search-result light"}>
+                {
+                    ((books.length === 0 && authors.length === 0) || SearchQuery === undefined) ?
+                        <>
+                            <h3>No Results for “{SearchQuery}”</h3>
+                            <div className='side-title'>browse by <Link className='link' to='/books'>Books</Link></div>
 
-            {
-                (books.length === 0 && authors.length === 0) || SearchQuery === undefined ?
-                    <>
-                        <img className="searchImg" src={notFoundImg} alt="not found" />
-                        <p className={theme === 'light' ? "alert light" : "alert"}>we recommend you to search by book's title, author's name or publisher</p>
-                    </>
-                    :
-                    null
-            }
+                            <div className="genres">
+                                <div className='side-title'>or by browse genres</div>
+                                <GenreList />
+                            </div>
 
-            {
-                books.length !== 0 ?
-                    <>
-                        <h2 className={theme === 'light' ? "header light" : "header"}>Books</h2>
-                        {
-                            books.map((item) => (
-                                <BookCard
-                                    key={item.id}
-                                    BookTitle={item.bookTitle}
-                                    src={item.bookCover}
-                                    AuthorName={item.author != null ? item.author.name : null}
-                                    page={`/genre/${item.genre != null ? item.genre.id : null}/books/${item.id}`}
-                                />
-                            ))
-                        }
-                    </>
-                    :
-                    null
-            }
+                        </>
+                        :
+                        <h3>Results for “{SearchQuery}”</h3>
+                }
 
-            {authors.length !== 0 ?
-                <>
-                    <h2 className={theme === 'light' ? "header light" : "header"}>Authors</h2>
-                    <div className="Cards">
-                        {
-                            authors.map((item) => (
-                                <AuthorCard
-                                    key={item.id}
-                                    src={item.pictureUrl}
-                                    name={item.name}
-                                    description={item.bio}
-                                    dateOfBirth={item.dateOfBirth.substring(0, 4)}
-                                    dateOfDeath={item.dateOfDeath != null ? item.dateOfDeath.substring(0, 4) !== "0001" ? item.dateOfDeath.substring(0, 4) : "" : ""}
-                                    page={`/Authors/${item.id}`}
-                                />
-                            ))
-                        }
-                    </div>
-                </>
-                :
-                null
-            }
+                {
+                    books.length !== 0 ?
+                        <>
+                            <h2 className="header">Books <span className='results-number'>{bookxPagination.totalCount} results</span></h2>
+                            <div className="books-list">
+                                {
+                                    books.map((item) => (
+                                        <BookCard
+                                            key={item.id}
+                                            data={item}
+                                        />
+                                    ))
+                                }
+                            </div>
+
+                            <div className='nav-buttons'>
+                                <button
+                                    className='nav-btn previous'
+                                    onClick={() => setBookPageNumber(booksPageNumber - 1)}
+                                    disabled={!bookxPagination.hasPrevious}
+                                >
+                                    Previous
+                                </button>
+
+                                <button
+                                    className='nav-btn next'
+                                    onClick={() => setBookPageNumber(booksPageNumber + 1)}
+                                    disabled={!bookxPagination.hasNext}
+                                >
+                                    Next
+                                </button>
+                            </div>
+
+                        </>
+                        :
+                        null
+                }
+
+                {
+                    authors.length !== 0 ?
+                        <>
+                            <h2 className="header">Authors <span className='results-number'>{authorxPagination.totalCount} results</span></h2>
+                            <div className="books-list">
+                                {
+                                    authors.map((item) => (
+                                        <AuthorCard
+                                            key={item.id}
+                                            data={item}
+                                        />
+                                    ))
+                                }
+                            </div>
+
+                            <div className='nav-buttons'>
+                                <button
+                                    className='nav-btn previous'
+                                    onClick={() => setAuthorPageNumber(authorsPageNumber - 1)}
+                                    disabled={!authorxPagination.hasPrevious}
+                                >
+                                    Previous
+                                </button>
+
+                                <button
+                                    className='nav-btn next'
+                                    onClick={() => setAuthorPageNumber(authorsPageNumber + 1)}
+                                    disabled={!authorxPagination.hasNext}
+                                >
+                                    Next
+                                </button>
+                            </div>
+                        </>
+                        :
+                        null
+                }
+            </div>
+
 
         </div>
     )
